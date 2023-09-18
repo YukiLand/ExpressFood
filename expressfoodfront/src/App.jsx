e7f0304cfbb1705bc839e564975ddb6e4f58151a
EdisonMASSENA <massenaedison0@outlook.fr>
2023-09-18 17:39:17 +0200

    accueil

--- expressfoodfront/src/App.jsx
+++ expressfoodfront/src/App.jsx
@@ -1,20 +1,32 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+import './App.css'
 import Layout from './components/Layout/Layout'
 import Home from './components/Home/Home'
+
 
 function App() {
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />} >
-          <Route index element={<Home />} />
-          {/* <Route path="about" element={<About />} /> */}
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />} >
+            <Route index element={<Home />} />
+            {/* <Route path="about" element={<About />} /> */}
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
