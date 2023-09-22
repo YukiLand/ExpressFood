63cfbc005f7fdfe7d73a5f676e7131b73c53629e
Antoine Lecoffre <antoine.lecoffre@orange.fr>
2023-09-22 10:56:42 +0200

    blablabla

--- expressfoodfront/src/components/Layout/Layout.jsx
+++ expressfoodfront/src/components/Layout/Layout.jsx
@@ -1,14 +1,11 @@
 import { Button, IconButton } from "@mui/material";
 import { Link, Outlet, useLocation } from "react-router-dom";
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import { useEffect, useState } from "react";
 
 import "./Layout.css";
 
-
 function Layout() {
-
-
   const location = useLocation();
 
   const currentRoute = location.pathname;
@@ -16,26 +13,45 @@ function Layout() {
   const [logBtn, setLogBtn] = useState(true);
 
   useEffect(() => {
-    setLogBtn(!currentRoute.startsWith('/product') && !currentRoute.startsWith('/detail') && currentRoute !== '/cart' && currentRoute !== '/payment');
+    setLogBtn(
+      !currentRoute.startsWith("/product") &&
+        !currentRoute.startsWith("/detail") &&
+        currentRoute !== "/cart" &&
+        currentRoute !== "/payment"
+    );
   }, [currentRoute]);
 
   return (
     <>
       <div className="navbar">
-        <Link className="logo-link" to="/"><img src="/src/assets/logo.png" className="logo" />EXPRESS FOOD</Link>
+        <Link className="logo-link" to="/">
+          <img src="/src/assets/logo.png" className="logo" />
+          EXPRESS FOOD
+        </Link>
         <div cassName="log-grp">
-          {logBtn &&
+          {logBtn && (
             <>
-              <Link className="log-btn" to="/connexion"><Button variant="outlined">Connexion</Button></Link>
-              <Link className="log-btn" to="/inscription"><Button variant="outlined">Inscription</Button></Link>
+              <Link className="log-btn" to="/connexion">
+                <Button variant="outlined">Connexion</Button>
+              </Link>
+              <Link className="log-btn" to="/inscription">
+                <Button variant="outlined">Inscription</Button>
+              </Link>
             </>
-          }
-          {!logBtn &&
-          <>
-            <Link className="log-btn" to="/product"><Button variant="outlined">Plats & Desserts</Button></Link>
-            <Link className="log-btn" to="/cart"><IconButton > <ShoppingCartIcon /> </IconButton></Link>
-          </>
-          }
+          )}
+          {!logBtn && (
+            <>
+              <Link className="log-btn" to="/product">
+                <Button variant="outlined">Plats & Desserts</Button>
+              </Link>
+              <Link className="log-btn" to="/cart">
+                <IconButton>
+                  {" "}
+                  <ShoppingCartIcon />{" "}
+                </IconButton>
+              </Link>
+            </>
+          )}
         </div>
       </div>
       <Outlet />
